3e9a90a3033bad2114499c121725055b0d044e35
Karin Ikenaga <57020115+Usag1@users.noreply.github.com>
2020-07-23 20:18:46 +0200

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,38 @@
+# test_blog.Vue
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+
+
+<html>
+<transition>
+<router-link to="/foo">Go to Foo</router-link>
+</transition>
+</html>
+
+Vue.use(Vuetify)
+
+const opts = {}
+
+
+<template>
+<div>
+
+</div>
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+export default new Vuetify(opts)
